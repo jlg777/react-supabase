465e70409a5d96a51f70a09bb52a2236d5113c7d
= <=>
2025-03-10 12:41:36 -0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+<<<<<<< HEAD
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
+=======
+import { BrowserRouter } from 'react-router'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+>>>>>>> 568b175 (update)
 )
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
+<<<<<<< HEAD
 import react from '@vitejs/plugin-react-swc'
+=======
+import react from '@vitejs/plugin-react'
+>>>>>>> 568b175 (update)
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
+=======
+  server: {
+    port: 3001 // Cambia este valor al puerto que desees
+  }
+>>>>>>> 568b175 (update)
 })
