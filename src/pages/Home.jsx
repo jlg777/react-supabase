bb51b71b06b36e2bbd2dabb3af18a8b46ea08fe4
= <=>
2025-03-12 13:16:45 -0300

    link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
 //import { supabase } from '@supabase/supabase-js'
 //import { supabase } from '../db/db'
 
+import { Link } from 'react-router-dom'
+
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <>
+      <div>Home</div>
+      <li>
+        <Link to="/login">login</Link>
+      </li>
+    </>
+  )
 }
 export default Home
